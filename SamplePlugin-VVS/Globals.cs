9048224378a8f831e12863db24376edc21bb69da
Dimmae <Dimmae@users.noreply.github.com>
2019-01-12 09:19:19 -0500

    Stripped

--- SamplePlugin-VVS/Globals.cs
+++ SamplePlugin-VVS/Globals.cs
@@ -7,19 +7,15 @@ namespace SamplePluginVVS
 {
 	public static class Globals
 	{
+		public static string PluginName { get; private set; }
+		public static PluginHost Host { get; private set; }
+		public static CoreManager Core { get; private set; }
+
 		public static void Init(string pluginName, PluginHost host, CoreManager core)
 		{
 			PluginName = pluginName;
-
 			Host = host;
-
 			Core = core;
-		}
-
-		public static string PluginName { get; private set; }
-
-		public static PluginHost Host { get; private set; }
-
-		public static CoreManager Core { get; private set; }
+		}		
 	}
 }
